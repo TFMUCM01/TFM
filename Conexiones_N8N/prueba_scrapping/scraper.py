daf02a414776522bc54bb6d1f49c15c4e3cd56f3
PieroRios2000 <priosdc@hotmail.com>
2025-08-04 10:51:54 +0200

    log_error_2

--- Conexiones_N8N/prueba_scrapping/scraper.py
+++ Conexiones_N8N/prueba_scrapping/scraper.py
@@ -54,5 +54,6 @@ def extraer_titulares(snapshot_url, fecha_str, fuente=None):
 
 # Guarda errores en archivo log
 def log_error(mensaje):
-    with open("scraping_log.txt", "a", encoding="utf-8") as f:
+    with open("scraping_log.txt", "a", errors="replace") as f:
         f.write(f"{datetime.now()} - {mensaje}\n")
+
