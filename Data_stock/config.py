76cf5188ca791dd61f3bd193fbb804e383c1cf71
Marco Mendieta <marcomendieta08@gmail.com>
2025-08-27 17:15:15 +0200

    datos y predicciones texto

--- Data_stock/config.py
+++
@@ -1,9 +0,0 @@
-# Snowflake connection details
-SNOWFLAKE_CONFIG = {
-    'user': 'tfmgrupo4',
-    'password': 'TFMgrupo4ucm01_01#',
-    'account': 'WYNIFVB-YE01854',          # ejemplo: xy12345.eu-central-1
-    'warehouse': 'TFM_WH',
-    'database': 'DATASTOCK_AUTOMATE',
-    'schema': 'STOCK'
-}
